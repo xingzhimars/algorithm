afc6eebf1ba228296e7a0f3205fd5629575a66a5
gy0117 <geyan0117@gmail.com>
2024-11-21 18:34:29 +0800

    【listnode】82. Remove Duplicates from Sorted List II

--- List/lc_82/main.go
+++ List/lc_82/main.go
@@ -5,24 +5,55 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func deleteDuplicates(head *ListNode) *ListNode {
+// 这道题目是有重复的元素，就不能选，而不是选一个
+// 那就是说需要判断 cur.Val 是否等于 cur.Next.Val，不等的话，就可以选cur
+func deleteDuplicatesErr(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
 	dummy := &ListNode{
-		Val: 110,
+		Val: -110,
 	}
 	dummy.Next = head
 
-	tail := dummy
-	for tail.Next != nil {
-		p1 := tail.Next.Next
-		for p1 != nil && p1.Val == tail.Next.Val {
-			p1 = p1.Next
+	pre := dummy
+	cur := head
+
+	for cur != nil {
+		for cur.Next != nil && cur.Val == cur.Next.Val {
+			cur = cur.Next
 		}
-		if tail.Next.Next == p1 {
-			// 中间只有一个值
-			tail = tail.Next
-		} else {
-			tail.Next = p1
-		}
+		cur = cur.Next
+
+		pre.Next = cur
+		pre = cur
+		cur = cur.Next
 	}
+	return dummy.Next
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	dummy := &ListNode{
+		Val: -110,
+	}
+	dummy.Next = head
+
+	pre := dummy
+	cur := head
+
+	for cur != nil {
+		if cur.Next == nil || cur.Val != cur.Next.Val {
+			pre.Next = cur
+			pre = cur
+		}
+		for cur.Next != nil && cur.Val == cur.Next.Val {
+			cur = cur.Next
+		}
+		cur = cur.Next
+	}
+	pre.Next = nil
 	return dummy.Next
 }
